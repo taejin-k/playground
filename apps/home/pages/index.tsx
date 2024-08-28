fce36f24bd332bdf040518db405b702709efa558
arko-tj <arko@futurewiz.co.kr>
2024-08-28 09:56:35 +0900

    feature: 보안으로 인한 구글 웹폰트 사용 제거

--- apps/home/pages/index.tsx
+++ apps/home/pages/index.tsx
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -12,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className}`}>
+      <main>
         <h1>Home</h1>
       </main>
     </>
